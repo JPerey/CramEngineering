b834999dbd638fde7d8ec8f018bdd69781d537e7
Jam Jam James Perey <jamjam.perey@gmail.com>
2022-11-26 09:26:51 -0800

    UPDATE: added first view/ template. Added gitignore for env file

--- cramengineering/formulas_database/views.py
+++ cramengineering/formulas_database/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(requests):
+    context = {}
+
+    return render(requests, "formulas_database/formulas-home.html", context)

b1ae87a4459643c89120935348ace819989ebde4
Jam Jam James Perey <jamjam.perey@gmail.com>
2022-11-25 19:58:09 -0800

    UPDATE: moved secret keys to env file

--- cramengineering/cramengineering/settings.py
+++ cramengineering/cramengineering/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=$2kp35%s78(kc&7nz2(6&kn8xu)_fj41@i-_0jh22_3o%co20"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    'formulas_database.apps.FormulasDatabaseConfig',
 ]
 
 MIDDLEWARE = [
